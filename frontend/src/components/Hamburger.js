a41763546bf1a12f5b7037f1c1dd2d5c9821b171
lucasrpatten <lucasrpattendev@gmail.com>
2023-02-13 12:02:11 -0700

    Added Prettier Formatter

---
+++ frontend/src/components/Hamburger.js
@@ -0,0 +1,8 @@
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+function Nav() {
+  return <></>;
+}
+
+export default Nav;
---
+++ frontend/src/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+
+import App from "./App";
+import Prices from "./pages/pricing";
+
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import ScrollToTop from "./components/Scroll";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ScrollToTop>
+        <div className="bg-dark-blue">
+          <Nav />
+          <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/Prices" element={<Prices />} />
+          </Routes>
+          <Footer />
+        </div>
+      </ScrollToTop>
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
