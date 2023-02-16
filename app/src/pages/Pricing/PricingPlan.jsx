e69e322f6681bd5145e23c8bfb3ce7effd76d810
lucasrpatten <lucasrpattendev@gmail.com>
2023-02-15 21:47:34 -0700

    Contact Form Complete

--- app/src/pages/Pricing/PricingPlan.jsx
+++ app/src/pages/Pricing/PricingPlan.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
 
 const PricingPlan = (props) => {
   const [styles, setStyles] = React.useState(props.styles);
@@ -11,18 +12,23 @@ const PricingPlan = (props) => {
   }
 
   return (
-    <motion.div /*whileHover={{ scale: 1.01, y: upMovement}}*/ className={styles}>
-      <p>{props.name}</p>
-      <h3 className="text-3xl">{props.price}</h3>
-      <button className="rainbow-button">
-        <div className="rainbow-button-inside">Choose Plan</div>
-      </button>
-      <ul className="mt-5 leading-loose">
-        {props.information.map((text, index) => (
-          <li key={`info_${index}`}>{text}</li>
-        ))}
-      </ul>
-    </motion.div>
+
+      <motion.div
+        // whileHover={{ scale: 1.01, y: upMovement }}
+        className={styles}
+      >
+        <p>{props.name}</p>
+        <h3 className="text-3xl">{props.price}</h3>
+        <button className="rainbow-button">
+          <div className="rainbow-button-inside">Choose Plan</div>
+        </button>
+        <ul className="mt-5 leading-loose">
+          {props.information.map((text, index) => (
+            <li key={`info_${index}`}>{text}</li>
+          ))}
+        </ul>
+      </motion.div>
+
   );
 };
 
