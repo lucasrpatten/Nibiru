91d8e1d748d27adbaea48dd627404fa75c2c18d3
lucasrpatten <lucasrpattendev@gmail.com>
2023-02-15 21:47:34 -0700

    Contact Form Complete

--- app/src/pages/Company/CustomerSupport/ContactInput.jsx
+++ app/src/pages/Company/CustomerSupport/ContactInput.jsx
@@ -1,11 +1,92 @@
-import React from "react";
+import React, { useState } from "react";
 
+const ContactInput = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [supportType, setSupportType] = useState("--- Select ---");
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!firstName || !lastName || !email || supportType === "--- Select ---") {
+      alert("Please fill in all the required fields.");
+      return;
+    }
+    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+      alert("Please enter a valid email address.");
+      return;
+    }
+    // Submit the form data to the server here
+    alert("Form submitted successfully!");
+  };
 
-const ContactInput = (props) => {
   return (
-      <div>
+    <>
+      <div className="w-full lg:w-[48%] flex flex-col">
+        <label className="text-white" htmlFor="first-name">
+          First Name
+        </label>
+        <input
+          className="w-100%"
+          name="first-name"
+          placeholder="John"
+          value={firstName}
+          onChange={(event) => setFirstName(event.target.value)}
+          required
+        />
       </div>
+      <div className="w-full lg:w-[48%] flex flex-col">
+        <label className="text-white" htmlFor="last-name">
+          Last Name
+        </label>
+        <input
+          className="w-100%"
+          name="last-name"
+          placeholder="Doe"
+          value={lastName}
+          onChange={(event) => setLastName(event.target.value)}
+          required
+        />
+      </div>
+      <div className="w-full flex flex-col">
+        <label className="text-white" htmlFor="email">
+          Email
+        </label>
+        <input
+          className="w-100%"
+          name="email"
+          placeholder="Email"
+          value={email}
+          type="email"
+          onChange={(event) => setEmail(event.target.value)}
+          required
+        />
+      </div>
+      <div className="w-full flex flex-col">
+        <label className="text-white" htmlFor="supportType">
+          Support Type
+        </label>
+        <select
+          className="w-100%"
+          name="supportType"
+          id="supportType"
+          value={supportType}
+          onChange={(event) => setSupportType(event.target.value)}
+          required
+        >
+          <option>--- Select ---</option>
+          <option>Scheduling, Rescheduling, Cancellations</option>
+          <option>Pricing, Refunds, Payments</option>
+          <option>Legal, Judiciary</option>
+          <option>General Questions</option>
+          <option>Complaints</option>
+          <option>Other</option>
+        </select>
+      </div>
+      <button type="submit" onClick={handleSubmit}>
+        Submit
+      </button>
+    </>
   );
 };
 
--- app/src/pages/Company/CustomerSupport/CustomerSupport.jsx
+++ app/src/pages/Company/CustomerSupport/CustomerSupport.jsx
@@ -8,15 +8,16 @@ const CustomerSupport = () => {
       <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }} className="p-5 lg:px-32 md:p-10 md:px-20">
-          <div className="hidden lg:h-[80vh] lg:flex flex-col items-center justify-center text-white">
-            <h2 className="text-2xl mb-10">
-              Get Assistance From our World Class, Award Winning Customer
-              Support Team
-            </h2>
-          </div>
-          <div className="flex flex-col-reverse items-center">
-          <ContactInput />
+        exit={{ opacity: 0 }}
+        className="p-5 lg:px-32 md:p-10 md:px-18 "
+      >
+        <div className="mt-[max(10vw,60px)] lg:flex flex-col items-center justify-center text-white">
+          <h2 className="text-2xl text-center mb-[min(15vw,200px)]">
+            Get Assistance From our Award-Winning Customer Support Team
+          </h2>
+        </div>
+                  <div className="flex flex-row flex-wrap justify-between items-center w-100vw content-between">
+            <ContactInput />
           </div>
       </motion.div>
     </>
