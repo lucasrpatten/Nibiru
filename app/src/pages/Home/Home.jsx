fb356184ce25d47f1cf74a04e12092f39d08ba70
lucasrpatten <lucasrpattendev@gmail.com>
2023-02-17 10:01:46 -0700

    Added Enviroment Graphics

--- app/src/pages/Home/Home.jsx
+++ app/src/pages/Home/Home.jsx
@@ -4,8 +4,14 @@ import { motion, AnimatePresence } from "framer-motion";
 import WhyNibiru from "./WhyNibiru";
 import Intro from "./Intro";
 import CompanyStats from "./CompanyStats";
-import EnviromentFriendly from "./EnviromentFriendly";
-import EarthView from "../../images/earth_from_window.png"
+import EnvironmentFriendly from "./EnvironmentFriendly";
+import TreeLogo from "./tree_logo.png";
+import LeafLogo from "./leaf_logo.png";
+import EarthLeafLogo from "./earth_leaf_logo.png";
+import EarthView from "./earth_from_window.png";
+import RhinoLogo from "./rhino_logo.png";
+import PlantRocketLogo from "./plant_rocket_logo.png";
+import RecyclingLogo from "./recycling_logo.png";
 
 const Home = () => {
   return (
@@ -23,10 +29,24 @@ const Home = () => {
         />
         <Comparable image={fillerImage} />
         <WhyNibiru image={fillerImage} />
-        <EnviromentFriendly image={fillerImage} />
+        <EnvironmentFriendly
+          // Photoshop these so they all have the same color background (#d7d2cf or #e6e5e5)
+          image1={TreeLogo}
+          alt1="tree logo"
+          image2={EarthLeafLogo}
+          alt2="earth-leaf-logo"
+          image3={LeafLogo}
+          alt3="leaf-logo"
+          image4={RhinoLogo}
+          alt4="rhino-logo"
+          image5={PlantRocketLogo}
+          alt5="plant-rocket-logo"
+          image6={RecyclingLogo}
+          alt6="recycling-logo"
+        />
       </div>
     </motion.div>
   );
-}
+};
 
 export default Home;
