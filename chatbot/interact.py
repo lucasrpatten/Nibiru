6f5b53a7349a5ccf47359c257650769075fbaddd
lucasrpatten <lucasrpattendev@gmail.com>
2023-02-18 16:24:15 -0700

    training

--- chatbot/interact.py
+++ chatbot/interact.py
@@ -7,6 +7,7 @@ import tensorflow as tf
 import tensorflowjs as tfjs
 from nltk.stem import WordNetLemmatizer
 from tensorflow import keras
+import subprocess
 
 # Download required NLTK data
 nltk.download('punkt')
@@ -27,6 +28,8 @@ stemmer = nltk.PorterStemmer()
 tokenizer = nltk.WordPunctTokenizer()
 
 # Create function to process user input
+
+
 def process_input(input_text):
     # Tokenize and lemmatize the input
     tokens = tokenizer.tokenize(input_text)
@@ -39,6 +42,8 @@ def process_input(input_text):
     return input_vector
 
 # Define function to generate chatbot response
+
+
 def generate_response(input_text):
     # Process the input
     input_vector = process_input(input_text)
@@ -46,10 +51,15 @@ def generate_response(input_text):
     # Get predicted probabilities for each tag
     results = model.predict(np.array([input_vector]))
     results = np.squeeze(results)
-
+    tag = ""
     # Choose the tag with the highest probability
-    tag_index = np.argmax(results)
-    tag = tags[tag_index]
+    print(np.max(results))
+    tag = ""
+    if np.max(results) > 0.53:
+        tag_index = np.argmax(results)
+        tag = tags[tag_index]
+    else:
+        tag = 'default'
 
     # Choose a random response from the tag's list of responses
     responses = []
@@ -58,15 +68,52 @@ def generate_response(input_text):
             responses = intent['responses']
     response = random.choice(responses)
 
-    return response
+    return response, tag
 
-# Run the chatbot
-while True:
-    # Get user input
-    input_text = input("You: ")
 
-    # Generate chatbot response
-    response = generate_response(input_text)
+def main():
+    # Run the chatbot
+    while True:
+        # Get user input
+        input_text = input("You: ")
 
-    # Print response
-    print("Bot: " + response)
+        # Generate chatbot response
+        response, tag_1 = generate_response(input_text)
+
+        correct = input(f"Does this fall under the tag of {tag_1}? (y/n): ")
+        json_data = ""
+        with open('trainingdata.json', 'r+') as file:
+            if correct == "y":
+                json_data = json.load(file)
+                tag_index = [i for i in range(len(json_data["intents"])) if json_data["intents"][i]["tag"] == tag_1][0]
+                json_data["intents"][tag_index]["patterns"].append(input_text)
+            elif correct == "n":
+                json_data = json.load(file)
+                correct_tag = input("What would the correct tag have been? ")
+                if correct_tag == "":
+                    print("You have chosen not to deal with this input right now")
+                else:
+                    try:
+                        tag_index = [i for i in range(len(json_data["intents"])) if json_data["intents"][i]["tag"] == correct_tag][0]
+                        json_data["intents"][tag_index]["patterns"].append(input_text)
+                    except IndexError:
+                        add_tag = input(f"The tag {correct_tag} does not exist in the training data. Would you like to add it? (y/n): ")
+                        if add_tag == "y":
+                            json_data["intents"].append({"tag": correct_tag, "patterns": [input_text]})
+                    if random.randint(0, 9) == 0:
+                        subprocess.run(["python", "model_train.py"])
+
+            elif correct == "":
+                print("You have chosen not to deal with this input right now")
+            else:
+                print("Not an answer lmao lol bad")
+        with open('trainingdata.json', 'w') as file:
+            json.dump(json_data, file, indent=2)
+
+        # Print response
+        print("Bot: " + response)
+        
+
+
+if __name__ == '__main__':
+    main()
