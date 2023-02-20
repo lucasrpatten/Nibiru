91fd4de48b2c4fc76c56241c69cc7ff058aacbea
lucasrpatten <lucasrpattendev@gmail.com>
2023-02-19 23:03:26 -0700

    updated training model using stanford glove

--- chatbot/interact.py
+++ chatbot/interact.py
@@ -1,53 +1,87 @@
 import json
 import numpy as np
 import tensorflow as tf
-import tensorflowjs as tfjs
 import tensorflow_text as text
-
-# Load the saved model
-model = tf.keras.models.load_model('chatbot_model.h5')
+from keras.models import load_model
+import tensorflowjs as tfjs
 
 # Load the vocabulary
-with open('./vocab_config.json') as f:
+with open('vocab_config.json') as f:
     config = json.load(f)
-with open('./vocab.json') as f:
-    vocab_list = json.load(f)
+with open('intents.json') as f:
+    data = json.load(f)
 vocab = tf.keras.layers.experimental.preprocessing.TextVectorization.from_config(config)
-# Load the tags
+
+# Load the model
+model = load_model('chatbot_model.h5')
+
+# Load the label encoder
 with open('tags.json') as file:
-    tags = json.load(file)
+    labels = json.load(file)
+label_encoder = {label: i for i, label in enumerate(labels)}
 
-# Define a function to process the user's input and get the model's prediction
-def get_prediction(input_text):
-    # Convert the input text to a sequence of integers using the vocabulary
-    input_seq = vocab(np.array([input_text]))
-    # Get the model's prediction for the input sequence
-    prediction = model.predict(input_seq)[0]
-    # Get the tag with the highest predicted probability
-    tag_idx = np.argmax(prediction)
-    tag = tags[tag_idx]
-    # Return the predicted tag and probability
-    return tag, prediction[tag_idx]
+# Define a function to preprocess input text
 
-# Define a loop to get user input and respond with the model's prediction
-print('Chatbot is ready. Type "exit" to end the conversation.')
-while True:
-    # Get user input
-    user_input = input('You: ')
-    if user_input.lower() == 'exit':
-        break
-    # Get the model's prediction for the user input
-    tag, prob = get_prediction(user_input)
-    # If the probability is below a threshold, treat it as unknown intent
-    threshold = 0.3
-    if prob < threshold:
-        tag = 'unknown'
-    # Look up the appropriate response for the predicted tag
-    for intent in data['intents']:
-        if intent['tag'] == tag:
-            responses = intent['responses']
+
+def preprocess_text(text):
+    return vocab(np.array([text])).numpy()[0]
+
+# Define a function to predict the intent of a given text
+
+
+def predict_intent(query):
+    # Preprocess the text
+    query = preprocess_text(query)
+
+    # Make a prediction
+    prediction = model.predict(np.array([query]))
+
+    # Get the predicted label
+    label_idx = np.argmax(prediction)
+    label = labels[label_idx]
+
+    # Get the confidence score
+    confidence = prediction[0][label_idx]
+    print(f"query: {query}\nprediction: {label}\nprediction: {prediction}\nlabel: {label}\nlabel_idx: {label_idx}\nconfidence: {confidence}")
+
+    return label, confidence
+
+# Define a function to get a response for a given intent
+
+
+def get_response(intent):
+    for example in data['intents']:
+        if example['tag'] == intent:
+            response = np.random.choice(example['responses'])
+            return response
+
+# Define a function to start the chatbot
+
+
+def start_chatbot():
+    print("Welcome to the chatbot! Type 'exit' to end the conversation.")
+    while True:
+        # Get user input
+        user_input = input("You: ")
+
+        # Check if the user wants to exit
+        if user_input.lower() == 'exit':
             break
-    # Select a random response from the list of possible responses
-    response = np.random.choice(responses)
-    # Print the response
-    print('Chatbot:', response)
+
+        # Predict the intent of the user's input
+        intent, confidence = predict_intent(user_input)
+
+        # Check if the intent is above a certain confidence threshold
+        if confidence < 0.5:
+            print("Sorry, I didn't understand what you said. Can you please rephrase?")
+            continue
+
+        # Get a response for the predicted intent
+        response = get_response(intent)
+
+        # Print the response
+        print("Chatbot: " + response)
+
+
+# Start the chatbot
+start_chatbot()
--- chatbot/model_train.py
+++ chatbot/model_train.py
@@ -4,6 +4,11 @@ import tensorflow as tf
 import tensorflowjs as tfjs
 import tensorflow_text as text
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+from datetime import datetime
+import threading
+import time
+
 
 # Load the data
 with open('intents.json') as file:
@@ -12,23 +17,27 @@ with open('intents.json') as file:
 # Create lists for the input data and labels
 input_data = []
 labels = []
+tags = []
 label_encoder = {}
 for intent in data['intents']:
     for pattern in intent['patterns']:
         input_data.append(pattern)
         label = intent['tag']
+        tags.append(label)
         if label not in label_encoder:
             label_encoder[label] = len(label_encoder)
         labels.append(label_encoder[label])
 
 # Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(input_data, labels, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(input_data, labels, test_size=0.15)  # random_state=42)
 
 # Create a vocabulary
 VOCAB_SIZE = 10000
 encoder = text.WhitespaceTokenizer()
+max_len = max(len(seq) for seq in X_train + X_test)
+
 vocab = tf.keras.layers.experimental.preprocessing.TextVectorization(
-    max_tokens=VOCAB_SIZE, standardize="lower_and_strip_punctuation"
+    max_tokens=VOCAB_SIZE, output_sequence_length=max_len, standardize="lower_and_strip_punctuation"
 )
 vocab.adapt(X_train)
 
@@ -36,9 +45,28 @@ vocab.adapt(X_train)
 X_train = vocab(X_train)
 X_test = vocab(X_test)
 
-# Create the model
+# Load the GloVe word embeddings
+embeddings_index = {}
+with open('./glove_embeddings/glove.6B.100d.txt') as file:
+    for line in file:
+        word, coefs = line.split(maxsplit=1)
+        coefs = np.fromstring(coefs, 'f', sep=' ')
+        embeddings_index[word] = coefs
+
+# Prepare the embedding matrix
+word_index = dict(zip(vocab.get_vocabulary(), range(VOCAB_SIZE)))
+embedding_matrix = np.zeros((VOCAB_SIZE, 100))
+for word, i in word_index.items():
+    if i < VOCAB_SIZE:
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            embedding_matrix[i] = embedding_vector
+
+# Define the model
+max_len = X_train.shape[1]
 model = tf.keras.Sequential([
-    tf.keras.layers.Embedding(VOCAB_SIZE + 1, 64),
+    tf.keras.layers.Embedding(VOCAB_SIZE, 100, input_length=max_len,
+                              weights=[embedding_matrix], trainable=False),
     tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64)),
     tf.keras.layers.Dense(64, activation='relu'),
     tf.keras.layers.Dropout(0.5),
@@ -48,21 +76,49 @@ model = tf.keras.Sequential([
 # Compile the model
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
+# Define the log directory for TensorBoard
+log_dir = "./logs/fit/" + datetime.now().strftime("%Y%m%d-%H%M%S")
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+
 # Train the model
-model.fit(X_train, tf.keras.utils.to_categorical(y_train), epochs=80, batch_size=5,
-          validation_data=(X_test, tf.keras.utils.to_categorical(y_test)))
+history = model.fit(X_train, tf.keras.utils.to_categorical(y_train), epochs=70, batch_size=5,
+                    validation_data=(X_test, tf.keras.utils.to_categorical(y_test)),
+                    callbacks=[tensorboard_callback])
 
 # Save the model and vocab
 config = vocab.get_config()
-
-config = config["vocabulary"] = vocab.get_vocabulary()
+config["vocabulary"] = vocab.get_vocabulary()
 with open('./vocab_config.json', 'w') as f:
     json.dump(config, f)
 with open('tags.json', 'w') as file:
-    json.dump(list(set(labels)), file)
+    json.dump(list(set(tags)), file)
 
 # Save the model
 model.save('chatbot_model.h5')
 
 # Save the model in TensorFlow.js format
 tfjs.converters.save_keras_model(model, 'chatbot_model_js')
+
+# view = input("Would you like to view the graphs in matplotlib? (y/n): ")
+# if view.lower() in ["y", "yes"]:
+#     # Create a 2x1 subplot layout
+#     fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 8))
+
+#     # Plot the training and validation loss over the epochs in the first subplot
+#     ax1.plot(history.history['loss'], label='Training Loss')
+#     ax1.plot(history.history['val_loss'], label='Validation Loss')
+#     ax1.set_title('Model Loss')
+#     ax1.set_xlabel('Epoch')
+#     ax1.set_ylabel('Loss')
+#     ax1.legend()
+
+#     # Plot the training and validation accuracy over the epochs in the second subplot
+#     ax2.plot(history.history['accuracy'], label='Training Accuracy')
+#     ax2.plot(history.history['val_accuracy'], label='Validation Accuracy')
+#     ax2.set_title('Model Accuracy')
+#     ax2.set_xlabel('Epoch')
+#     ax2.set_ylabel('Accuracy')
+#     ax2.legend()
+
+#     # Show the plot
+#     plt.show()
