f29c870d1edcd537ffcb5544b943e5af64b84ee9
Catherine Bao <112794244+CatherineBao@users.noreply.github.com>
2023-03-08 12:12:11 -0700

    addded an image for mobile stuff

--- app/src/pages/Registration.jsx
+++ app/src/pages/Registration.jsx
@@ -3,7 +3,8 @@ import { motion } from "framer-motion";
 import ship from "./ship.png";
 import building from "./buidingSpace.png";
 import { Link } from "react-router-dom";
-import Banner from "./pageBanner.jpg"
+import Banner from "./pageBanner.jpg";
+import MBanner from "./bannerMobile.jpg"
 
 const Registration = () => {
   return (
@@ -14,7 +15,8 @@ const Registration = () => {
     >
       <div className="">
         <div className="w-full h-full flex flex-col items-center justify-center">
-            <img className="w-full h-full" src={Banner}/>
+            <img className="w-full h-full hidden md:block" src={Banner}/>
+            <img className="w-full h-full block md:hidden" src={MBanner}/>
         </div>
       </div>
     </motion.div>
