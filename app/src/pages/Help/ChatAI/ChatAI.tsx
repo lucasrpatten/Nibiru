ed1aadbdde7e5f29bca3c3ae78984a957eba9c42
lucasrpatten <lucasrpattendev@gmail.com>
2023-03-10 16:44:47 -0700

    Chat Fixed

--- app/src/pages/Help/ChatAI/ChatAI.tsx
+++ app/src/pages/Help/ChatAI/ChatAI.tsx
@@ -10,21 +10,17 @@ interface MessageProps {
   isBot: boolean;
 }
 const ChatMessage: React.FC<MessageProps> = (props) => {
-  let float_dir = props.isBot ? "float-left" : "float-right";
+  let float_dir = props.isBot ? "float-left max-w-[80%]" : "float-right max-w-[80%]";
+  let name = props.isBot ? "Nibiru Bot:" : "You:";
   return (
-    <div className="mb-10 w-full">
-      <div className={float_dir}>
-        <div className="w-full flex items-start justify-start gap-10">
-          {/* <img className="w-10 h-10 hidden md:block" src={profile}/> */}
-          <div className="flex flex-col justify-end">
-          <p className="text-white mb-3">Nibiru Bot:</p>
-          <div className="h-full bg-white rounded-xl text-dark-blue p-5">
+      <div className={`relative mb-1 w-full items-end gap-10`}>
+        <div className={`${float_dir} `}>
+          <p className="text-white mb-2">{name}</p>
+          <div className="h-full bg-white rounded-xl  text-dark-blue p-3">
             {props.message}
-            </div> <br />
           </div>
-          </div>
+        </div>
       </div>
-    </div>
   );
 };
 
@@ -46,7 +42,6 @@ const ChatAI: React.FunctionComponent = () => {
     );
   };
 
-
   // Define a function to preprocess the text
   const preprocessText = async (text: string): Promise<number[]> => {
     text = text.toLowerCase();
@@ -55,9 +50,9 @@ const ChatAI: React.FunctionComponent = () => {
     console.log(text_arr);
     const int_arr: any[] = [];
     let vocab_json = JSON.parse(token)["config"]["word_index"];
-    vocab_json = JSON.parse(vocab_json)
+    vocab_json = JSON.parse(vocab_json);
     for (const word of text_arr) {
-      const numerical_key = vocab_json[word]
+      const numerical_key = vocab_json[word];
       int_arr.push(numerical_key === undefined ? 0 : numerical_key);
     }
     return int_arr;
@@ -70,36 +65,38 @@ const ChatAI: React.FunctionComponent = () => {
     return model;
   };
 
-  const predictOutput = async (text: string) => {
+  const predictOutput = async (text: string, message: string) => {
     const inputSeq = await preprocessText(text);
     const paddedSeq = padSequences([inputSeq], 20);
     console.log(paddedSeq.toString());
     const prediction = model.predict(paddedSeq);
     console.log(prediction.dataSync());
     let most_likely = prediction.argMax(1).dataSync()[0];
-    console.log(most_likely);
     let certainty = prediction.dataSync()[most_likely];
-    console.log(certainty);
     most_likely = certainty > 0.65 ? most_likely + 1 : 0;
     let response_options = responses[most_likely];
     let new_response =
       response_options[Math.floor(Math.random() * response_options.length)];
-    console.log(new_response);
 
     // Add delay before bot response
     await new Promise((resolve) => setTimeout(resolve, 1000));
 
     setMessages(
-      [...messages, { message: new_response, isBot: true }].slice(-20)
+      [
+        ...messages,
+        { message: message, isBot: false },
+        { message: new_response, isBot: true },
+      ].slice(-20)
     );
   };
 
   const messageSent = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    let message = messageInput;
+    setMessages([...messages, { message: messageInput, isBot: false }]);
+    setMessageInput("");
     if (messageInput) {
-      setMessages([...messages, { message: messageInput, isBot: false }]);
-      setMessageInput("");
-      await predictOutput(messageInput);
+      await predictOutput(messageInput, message);
     }
   };
 
@@ -109,34 +106,47 @@ const ChatAI: React.FunctionComponent = () => {
 
   return (
     <>
-        <div className="md:p-14 w-full flex flex-col items-center justify-center p-10 bg-gray my-5 rounded-xl relative text-white">
-          <h2 className="p-8 text-2xl text-white text-center capitalize"> AI Customer Assistance </h2>
-          <div className="mt-5 w-full flex flex-col-reverse overscroll-contain overflow-anchor:auto overflow-y-scroll h-96 rounded-t-xl bg-dark-blue p-10">
-              <div className="flex flex-col">
-              <h2 className="text-lg text-light-gray text-center italic capitalize"> Our digital assistant is ready to assist you... </h2>
-                {messages.map((message, index) => (
-                    <ChatMessage
-                      message={message.message}
-                      isBot={message.isBot}
-                      key={index}
-                    />
-                  ))}
-              </div>
-            </div>
-          <form className="rounded-b-xl focus-within:border-teal md:flex items-between justify-between gap-8 w-full md:bg-white rounded-b-xl" onSubmit={messageSent}>
-            <input
-              required
-              type="text"
-              placeholder="Enter Message Here"
-              onChange={(e) => setMessageInput(e.target.value)}
-              value={messageInput}
-              className="focus:outline-none bg-white text-dark-blue md:mb-0 w-full md:w-3/4 py-4 px-10 p-1 font-bold md:rounded-full"
-            />
-            <div className="w-full md:w-1/6 p-1 font-bold rounded-b-xl md:rounded-br-xl bg-gradient-to-r from-teal !to-purple via-blue">
-              <input className="hover:cursor-pointer py-4 px-10 p-1 rounded-b-xl md:rounded-br-xl text-white w-full hover:bg-dark-blue/100 transition duration-300 bg-dark-blue/0" type="submit" value="Send" />
-            </div>
-          </form>
+      <div className="md:p-14 w-full flex flex-col items-center justify-center p-10 bg-gray my-5 rounded-xl relative text-white">
+        <h2 className="p-8 text-2xl text-white text-center capitalize">
+          {" "}
+          AI Customer Assistance{" "}
+        </h2>
+        <div className="mt-5 w-full flex flex-col-reverse overscroll-contain overflow-anchor:auto overflow-y-scroll h-96 rounded-t-xl bg-dark-blue p-10">
+          <div className="flex flex-col">
+            <h2 className="text-lg text-light-gray text-center italic capitalize">
+              {" "}
+              Our digital assistant is ready to assist you...{" "}
+            </h2>
+            {messages.map((message, index) => (
+              <ChatMessage
+                message={message.message}
+                isBot={message.isBot}
+                key={index}
+              />
+            ))}
+          </div>
         </div>
+        <form
+          className="rounded-b-xl focus-within:border-teal md:flex items-between justify-between gap-8 w-full md:bg-white rounded-b-xl"
+          onSubmit={messageSent}
+        >
+          <input
+            required
+            type="text"
+            placeholder="Enter Message Here"
+            onChange={(e) => setMessageInput(e.target.value)}
+            value={messageInput}
+            className="focus:outline-none bg-white text-dark-blue md:mb-0 w-full md:w-3/4 py-4 px-10 p-1 font-bold md:rounded-full"
+          />
+          <div className="w-full md:w-1/6 p-1 font-bold rounded-b-xl md:rounded-br-xl bg-gradient-to-r from-teal !to-purple via-blue">
+            <input
+              className="hover:cursor-pointer py-4 px-10 p-1 rounded-b-xl md:rounded-br-xl text-white w-full hover:bg-dark-blue/100 transition duration-300 bg-dark-blue/0"
+              type="submit"
+              value="Send"
+            />
+          </div>
+        </form>
+      </div>
     </>
   );
 };
